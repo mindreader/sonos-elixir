461cf52ac12f732724324d8ae2f6d59763b082d1
David McHealy <david@simplecast.com>
2020-04-25 11:09:04 -0400

    Example of serving a file for a speaker to play.

--- lib/sonos/router.ex
+++ lib/sonos/router.ex
@@ -4,6 +4,12 @@ defmodule Sonos.Router do
   plug :match
   plug :dispatch
 
+  match "/audio/:filename" do
+    file = "/home/toad/music/game/Switch vs Evil.mp3"
+
+    conn |> put_resp_content_type("audio/mpeg") |> send_file(200, file)
+  end
+
   match _ do
     {:ok, body, conn} = conn |> Plug.Conn.read_body()
 
