9e693c623dedc078ba657fbbb8b4f72281da952d
David McHealy <david.mchealy@gmail.com>
2024-12-05 08:09:25 -0500

    feature: Flake support.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Twilight Struggle Counter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+
+  let
+
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    start = {
+      type = "app";
+      program = (pkgs.writeShellScript "sonos-start" ''
+        ${pkgs.elixir}/bin/iex -S mix phx.server
+      '').outPath;
+    };
+  in with pkgs; {
+
+    # nix run .#start
+    apps.x86_64-linux.default = start;
+    apps.x86_64-linux.start = start;
+
+    # nix develop
+    devShells.x86_64-linux.default = pkgs.mkShell {
+
+      name = "ts-shell";
+      packages = [ pkgs.elixir pkgs.elixir-ls ];
+    };
+  };
+}
