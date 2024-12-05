decd8d66787d0e7d7872fc96d487e39ac6d33f5a
David McHealy <david.mchealy@gmail.com>
2024-12-05 08:09:38 -0500

    chore: Updating dependencies.

--- mix.exs
+++ mix.exs
@@ -22,12 +22,12 @@ defmodule SonosElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"},
-      {:httpoison, "~> 1.0"},
-      {:sweet_xml, "~> 0.6"},
-      {:xml_builder, "~> 2.0.0"},
-      {:flex_logger, "~> 0.2.0"},
-      {:timex, "~> 3.0"},
+      {:plug_cowboy, "~> 2.7"},
+      {:jason, "~> 1.4"},
+      {:sweet_xml, "~> 0.7"},
+      {:xml_builder, "~> 2.3.0"},
+      {:timex, "~> 3.7"},
+      {:httpoison, "~> 2.2.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
