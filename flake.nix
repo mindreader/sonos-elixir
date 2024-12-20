39bcf891a5d3a44334e78973141c69ff689875de
David McHealy <david.mchealy@gmail.com>
2024-12-20 04:47:14 -0500

    Revert "feature: nix run command."
    This reverts commit 169f15ded9b2d092d8eae7b90ddeb6c79e2b555b.
    
    This doesn't actually work. This is not appropriate for a run command.

--- flake.nix
+++ flake.nix
@@ -8,31 +8,24 @@
   outputs = { self, nixpkgs }:
 
   let
+
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
     start = {
       type = "app";
-      program =
-        let script = pkgs.writeShellApplication {
-          name = "sonos-start";
-          # needed for file watching.
-          runtimeInputs = [ pkgs.inotify-tools ];
-          text = ''
-            ${pkgs.elixir}/bin/mix deps.get
-            ${pkgs.elixir}/bin/iex -S ${pkgs.elixir}/bin/mix phx.server
-          '';
-        };
-
-        in "${script}/bin/sonos-start";
-      };
+      program = (pkgs.writeShellScript "sonos-start" ''
+        ${pkgs.elixir}/bin/iex -S mix phx.server
+      '').outPath;
+    };
   in with pkgs; {
 
-    # nix run .#start (or just nix run)
+    # nix run .#start
     apps.x86_64-linux.default = start;
     apps.x86_64-linux.start = start;
 
     # nix develop
     devShells.x86_64-linux.default = pkgs.mkShell {
+
       name = "ts-shell";
       packages = with pkgs; [ elixir elixir-ls inotify-tools ];
     };
